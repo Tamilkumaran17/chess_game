b136dcd0fc8dde7964b61a7f974108150cdd770f
tamilkumaran <tamilkumaran1494@gmail.com>
2024-03-14 09:35:24 +0530

    welcome

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ const handleStartGame = async (e) => {
         
         {showChessboard && (
           <>
-            <p className="user">Welcomeee: {username}</p>
+            <p className="user">Welcome: {username}</p>
             <div className="board-container">
               <Chessboard
                 position={game.fen()}
